3baa351fa667601f64a1e6396ce12dd2a6b8d022
linuxvip <525277891@qq.com>
2018-05-23 22:14:59 +0800

    settings

--- youth/settings.py
+++ youth/settings.py
@@ -157,10 +157,10 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'statics/')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static').replace('\\', '/'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR,'static/')
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static').replace('\\', '/'),
+#)
 
 
 REST_FRAMEWORK = {
